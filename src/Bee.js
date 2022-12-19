05d34cf0f15a07d90c3c62116625555b0e462a5c
Kevin Le <iamkevinvle@gmail.com>
2022-12-19 11:50:09 -0800

    implement all bee classes in ES6 syntax

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+
+  //in es6, methods belong inside the constructor function
+  constructor() {
+    //a call to super, along with the extends keyord seem necessary to form the inhertitance connection
+    super()
+    this.age = 5;
+    this.color = 'yellow'
+    this.job = 'Keep on growing'
+  }
 };
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor () {
+    //you can pass in arguments into super parameters
+    super()
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+
+  // HoneyMakerBee specific methods go here
+  makeHoney () {
+    this.honeyPot++
+  }
+  giveHoney () {
+    this.honeyPot--
+  }
 };
